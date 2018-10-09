fba3ab3768967525c014e7040891f2a2cf943b67
Alejandro Perdomo <aperdomobo@gmail.com>
2018-10-08 21:08:41 -0500

    update test

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,13 +3,14 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/spec.js' ],
+  specs: [ '../test/*.spec.js' ],
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
       args: ['--headless', '--disable-gpu']
     }
   },
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -3,5 +3,5 @@ import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter';
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     customProcessors: [DisplayProcessor]
-  }))
+  }));
 };
